0061cbba011abc375875692b12fe1fca5d88b608
Y0U5S3F <akim25486@gmail.com>
2025-03-04 11:41:53 +0100

    showTooltip: false,

--- src/components/Scheduler/Scheduler.tsx
+++ src/components/Scheduler/Scheduler.tsx
@@ -26,7 +26,7 @@ const Scheduler = ({
       zoom: 0,
       filterButtonState: 1,
       includeTakenHoursOnWeekendsInDayView: false,
-      showTooltip: true,
+      showTooltip: false,
       translations: undefined,
       ...config
     }),
